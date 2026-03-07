b5ef5edfc36002c32e52805440e7eec4d22e6efd
Torgo13 <t.d.walker42@gmail.com>
2026-03-07 11:03:28 +0000

    Updated version to 1.0.0
    Allow ShadingRateFragmentSize to be set in TwoPassRasterBlurRendererFeature
    Replaced Shader.Find() by adding a reference to the Shader in the ScriptableRendererFeature
    Fixed missing asmdef
    Fixed misplaced comma in package.json
    Fixed typos in SubShader Pass names

--- TwoPassBlur/Blur.shader
+++ TwoPassBlur/Blur.shader
@@ -7,7 +7,7 @@ Shader "Hidden/RGSample/2PassBlur"
 
         Pass
         {
-            Name "DonwSample Verttical"
+            Name "DownSample Vertical"
             ZTest Never 
             ZWrite OFf 
             Cull Off
@@ -25,25 +25,24 @@ Shader "Hidden/RGSample/2PassBlur"
 
             float _BlurSize;
             
-            static const int BLUR_SAMPLE_COUNT=3;
+            static const int BLUR_SAMPLE_COUNT = 3;
             static const float BLUR_KERNEL[BLUR_SAMPLE_COUNT] = {
                 0.25, 0.5, 0.25
             };
             static const float BLUR_KERNEL_OFFSET[BLUR_SAMPLE_COUNT] = {
-                -1,0,1
+                -1, 0, 1
             };
 
             half4 Fragment(Varyings input) : SV_Target 
             {
-                // return half4(1, 0, 0, 1);
                 UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
                 half2 scale = half2(_BlurSize, 0);
 
                 half4 color = 0;
-                for(int i=0; i<BLUR_SAMPLE_COUNT; i++)
+                for (int i = 0; i < BLUR_SAMPLE_COUNT; i++)
                 {
                     float2 ofs = BLUR_KERNEL_OFFSET[i] * scale;
-                    half4 blurColor = SAMPLE_TEXTURE2D(_BlitTexture, sampler_LinearClamp, input.texcoord+ofs);
+                    half4 blurColor = SAMPLE_TEXTURE2D(_BlitTexture, sampler_LinearClamp, input.texcoord + ofs);
                     color.rgb += blurColor.rgb * BLUR_KERNEL[i];
                 }
                 return color;
@@ -52,7 +51,7 @@ Shader "Hidden/RGSample/2PassBlur"
         }
         Pass
         {
-            Name "DonwSample Horizontal"
+            Name "DownSample Horizontal"
             ZTest Never 
             ZWrite OFf 
             Cull Off
@@ -70,12 +69,12 @@ Shader "Hidden/RGSample/2PassBlur"
 
             float _BlurSize;
 
-            static const int BLUR_SAMPLE_COUNT=3;
+            static const int BLUR_SAMPLE_COUNT = 3;
             static const float BLUR_KERNEL[BLUR_SAMPLE_COUNT] = {
                 0.25, 0.5, 0.25
             };
             static const float BLUR_KERNEL_OFFSET[BLUR_SAMPLE_COUNT] = {
-                -1,0,1
+                -1, 0, 1
             };
 
             half4 Fragment(Varyings input) : SV_Target 
@@ -84,10 +83,10 @@ Shader "Hidden/RGSample/2PassBlur"
                 float2 scale = float2(0, _BlurSize);
 
                 half4 color = 0; 
-                for(int i=0; i<BLUR_SAMPLE_COUNT; i++)
+                for (int i = 0; i < BLUR_SAMPLE_COUNT; i++)
                 {
                     float2 ofs = BLUR_KERNEL_OFFSET[i] * scale;
-                    half4 blurColor = SAMPLE_TEXTURE2D_LOD(_BlitTexture, sampler_LinearClamp, input.texcoord+ofs, 0);
+                    half4 blurColor = SAMPLE_TEXTURE2D_LOD(_BlitTexture, sampler_LinearClamp, input.texcoord + ofs, 0);
                     color.rgb += blurColor.rgb * BLUR_KERNEL[i];
                 }
 
--- TwoPassBlur/TwoPassBlurRendererFeature.cs
+++ TwoPassBlur/TwoPassBlurRendererFeature.cs
@@ -1,161 +1,168 @@
-using System;
+#if URP_COMPATIBILITY_MODE
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.RenderGraphModule.Util;
 using UnityEngine.Rendering.Universal;
 
-class BlurRenderPass : ScriptableRenderPass
+namespace PKGE
 {
-    static readonly string s_passName = "2 Pass Blur";
-    static readonly ProfilingSampler s_profilingSampler = new (s_passName);
-    
-    private RTHandle _tmpHandle;
-    private Material _material;
-    
-    private float _blurSize;
-    static readonly int s_blurSizeId = Shader.PropertyToID("_BlurSize");
-    
-    public class PassData
+    internal sealed
+    class BlurRenderPass : ScriptableRenderPass
     {
-        public Material material;
-        public TextureHandle cameraTarget;
-        public TextureHandle tmpHandle;
-        public float blurSize;
-    }
-    
-    public void Setup(Material material, float blurSize)
-    {
-        _material = material;
-        _blurSize = blurSize;
-    }
+        const string s_passName = "2 Pass Blur";
+        static readonly ProfilingSampler s_profilingSampler = new ProfilingSampler(s_passName);
 
-    public void CleanUp()
-    {
-        _tmpHandle?.Release();
-    }
-    
-    public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
-    {
-        RenderTextureDescriptor desc = cameraTextureDescriptor;
-        desc.depthBufferBits = 0;
-        desc.msaaSamples = 1;
-        RenderingUtils.ReAllocateHandleIfNeeded (ref _tmpHandle, desc);
-    }
-    
+        private RTHandle _tmpHandle;
+        private Material _material;
 
-    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-    {
-        if (_material == null)
-            return;
-        var cmd = CommandBufferPool.Get(s_passName);
-        using (new ProfilingScope(cmd, s_profilingSampler))
+        private float _blurSize;
+        static readonly int s_blurSizeId = Shader.PropertyToID("_BlurSize");
+
+        sealed
+        public class PassData
         {
-            cmd.Clear();
-            
-            RTHandle cameraTarget = renderingData.cameraData.renderer.cameraColorTargetHandle;
-            if (cameraTarget == null || cameraTarget.rt == null)
+            public Material material;
+            public TextureHandle cameraTarget;
+            public TextureHandle tmpHandle;
+            public float blurSize;
+        }
+
+        public void Setup(Material material, float blurSize)
+        {
+            _material = material;
+            _blurSize = blurSize;
+        }
+
+        public void CleanUp()
+        {
+            _tmpHandle?.Release();
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            RenderTextureDescriptor desc = cameraTextureDescriptor;
+            desc.depthBufferBits = 0;
+            desc.msaaSamples = 1;
+            RenderingUtils.ReAllocateHandleIfNeeded(ref _tmpHandle, desc);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (_material == null)
                 return;
-    
+            
+            var cmd = CommandBufferPool.Get(s_passName);
+            using (new ProfilingScope(cmd, s_profilingSampler))
+            {
+                cmd.Clear();
+
+                RTHandle cameraTarget = renderingData.cameraData.renderer.cameraColorTargetHandle;
+                if (cameraTarget == null || cameraTarget.rt == null)
+                    return;
+
+                // step 1: vertical pass
+                _material.SetFloat(s_blurSizeId, _blurSize);
+                Blitter.BlitCameraTexture(cmd, cameraTarget, _tmpHandle, _material, 0);
+
+                // step 2: horizontal pass
+                Blitter.BlitCameraTexture(cmd, _tmpHandle, cameraTarget, _material, 1);
+
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+            }
+
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            // ContextContainer (frameData) から UniversalResourceData を取得し、カメラターゲット(activeColorTexture)を取得   
+            var resourceData = frameData.Get<UniversalResourceData>();
+            TextureHandle cameraTarget = resourceData.activeColorTexture;
+            if (!cameraTarget.IsValid())
+                return;
+
+            // TextureHandle.GetDescriptor で TextureHandle から TextureDescriptor を取得し部分的に変更を加える
+            TextureDesc desc = cameraTarget.GetDescriptor(renderGraph);
+            desc.depthBufferBits = 0;
+            desc.msaaSamples = MSAASamples.None;
+            TextureHandle tmpHandle = renderGraph.CreateTexture(desc);
+
+#if true
+            // Unsafe Pass
+            using var builder =
+                renderGraph.AddUnsafePass<PassData>(s_passName, out PassData passData, s_profilingSampler);
+            builder.UseTexture(cameraTarget, AccessFlags.ReadWrite);
+            builder.UseTexture(tmpHandle, AccessFlags.ReadWrite);
+
+            // Set PassData
+            passData.cameraTarget = cameraTarget;
+            passData.tmpHandle = tmpHandle;
+            passData.blurSize = _blurSize;
+            passData.material = _material;
+
+            builder.SetRenderFunc(static (PassData data, UnsafeGraphContext ctx) => ExecutePass(data, ctx));
+
+            static void ExecutePass(PassData data, UnsafeGraphContext ctx)
+            {
+                CommandBuffer cmd = CommandBufferHelpers.GetNativeCommandBuffer(ctx.cmd);
+
+                // step 1: vertical pass
+                data.material.SetFloat(s_blurSizeId, data.blurSize);
+                Blitter.BlitCameraTexture(cmd, data.cameraTarget, data.tmpHandle, data.material, 0);
+
+                // step 2: horizontal pass
+                Blitter.BlitCameraTexture(cmd, data.tmpHandle, data.cameraTarget, data.material, 1);
+            }
+
+#else
+            // Unsafe Pass ユーティリティ関数バージョン
             // step 1: vertical pass
             _material.SetFloat(s_blurSizeId, _blurSize);
-            // Blit(cmd, cameraTarget, _tmpHandle, _material, 0);
-            Blitter.BlitCameraTexture(cmd, cameraTarget, _tmpHandle, _material, 0);
+            RenderGraphUtils.BlitMaterialParameters param1 = new(cameraTarget, tmpHandle, _material, 0);
+            renderGraph.AddBlitPass(param1);
             
             // step 2: horizontal pass
-            // Blit(cmd, _tmpHandle, cameraTarget, _material, 1);
-            Blitter.BlitCameraTexture(cmd, _tmpHandle, cameraTarget, _material, 1);
-            
-            context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
-        }
-        CommandBufferPool.Release(cmd);
-    }
-
-    public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
-    {
-        // ContextContainer (frameData) から UniversalResourceData を取得し、カメラターゲット(activeColorTexture)を取得   
-        var resourceData = frameData.Get<UniversalResourceData>();
-        TextureHandle cameraTarget = resourceData.activeColorTexture;
-        if (!cameraTarget.IsValid())
-            return;
-
-        // TextureHandle.GetDescriptor で TextureHandle から TextureDescriptor を取得し部分的に変更を加える
-        TextureDesc desc = cameraTarget.GetDescriptor(renderGraph);
-        desc.depthBufferBits = 0;
-        desc.msaaSamples = MSAASamples.None;
-        TextureHandle tmpHandle = renderGraph.CreateTexture(desc);
-        
-#if true
-        // Unsafe Pass
-        using var builder = renderGraph.AddUnsafePass<PassData>(s_passName, out PassData passData, s_profilingSampler);
-        builder.UseTexture(cameraTarget, AccessFlags.ReadWrite);
-        builder.UseTexture(tmpHandle, AccessFlags.ReadWrite);
-        
-        // Set PassData
-        passData.cameraTarget = cameraTarget;
-        passData.tmpHandle = tmpHandle;
-        passData.blurSize = _blurSize;
-        passData.material = _material;
-        
-        builder.SetRenderFunc((PassData data, UnsafeGraphContext ctx) => ExecutePass(data, ctx));
-        
-        static void ExecutePass(PassData data, UnsafeGraphContext ctx)
-        {
-            CommandBuffer cmd = CommandBufferHelpers.GetNativeCommandBuffer(ctx.cmd);
-
-            // step 1: vertical pass
-            data.material.SetFloat(s_blurSizeId, data.blurSize);
-            Blitter.BlitCameraTexture(cmd, data.cameraTarget, data.tmpHandle, data.material, 0);
-            
-            // step 2: horizontal pass
-            Blitter.BlitCameraTexture(cmd, data.tmpHandle, data.cameraTarget, data.material, 1);
-        }
- 
-#else
-        // Unsafe Pass ユーティリティ関数バージョン
-        // step 1: vertical pass
-        _material.SetFloat(s_blurSizeId, _blurSize);
-        RenderGraphUtils.BlitMaterialParameters param1 = new(cameraTarget, tmpHandle, _material, 0);
-        renderGraph.AddBlitPass(param1);
-        
-        // step 2: horizontal pass
-        RenderGraphUtils.BlitMaterialParameters param2 = new(tmpHandle, cameraTarget, _material, 1);
-        renderGraph.AddBlitPass(param2); 
+            RenderGraphUtils.BlitMaterialParameters param2 = new(tmpHandle, cameraTarget, _material, 1);
+            renderGraph.AddBlitPass(param2);
 #endif
-        
-     }
+        }
+    }
+
+    sealed
+    public class TwoPassBlurRendererFeature : ScriptableRendererFeature
+    {
+        private BlurRenderPass _pass;
+        private Material _material;
+
+        [SerializeField] private Shader _shader;
+        [SerializeField] private RenderPassEvent _passEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        [SerializeField, Range(0.0f, 0.1f)] private float _blurSize = 0.005f;
+
+        public float blurSize { get => _blurSize; set => _blurSize = value; }
+
+        public override void Create()
+        {
+            _pass = new BlurRenderPass();
+            _pass.renderPassEvent = _passEvent;
+            _material = new Material(_shader);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _pass.CleanUp();
+            DestroyImmediate(_material);
+            _material = null;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            UnityEngine.Assertions.Assert.IsNotNull(_material);
+            
+            _pass.Setup(_material, _blurSize);
+            renderer.EnqueuePass(_pass);
+        }
+    }
 }
-    
-public class TwoPassBlurRendererFeature : ScriptableRendererFeature
-{
-    private BlurRenderPass _pass;
-    private Material _material;
-    
-    [SerializeField]
-    private RenderPassEvent _passEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-    [SerializeField, Range(0.0f, 0.1f)]
-    private float _blurSize = 0.005f;
-
-    public override void Create()
-    {
-        _pass = new BlurRenderPass();
-        _pass.renderPassEvent = _passEvent;
-        _material = new Material(Shader.Find("Hidden/RGSample/2PassBlur"));
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        _pass.CleanUp();
-        DestroyImmediate(_material);
-        _material = null;
-    }
-
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
-        if (_material == null)
-            return;
-        _pass.Setup(_material, _blurSize);
-        renderer.EnqueuePass(_pass);
-    }
-}
+#endif // URP_COMPATIBILITY_MODE
